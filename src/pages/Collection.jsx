f6dc7773ac70d2916b4f5ddb9f6183bde0f542eb
Klement Tamás <klemitomi84@gmail.com>
2025-06-20 00:18:34 +0200

    change

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext';
 
 const Collection = () => {
   return (
     <div>
-      
+      <h1 style={{ color: 'red', fontSize: 40 }}>TESZT</h1>
+     
     </div>
   )
 }
 
-export default Collection
+export default Collection;
