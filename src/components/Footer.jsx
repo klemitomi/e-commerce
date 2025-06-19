22c16f820bb16fc424e43c8779216645e5c26219
Klement Tamás <klemitomi84@gmail.com>
2025-06-19 23:38:19 +0200

    Add files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+
+        <div>
+            <img src={assets.logo} className='mb-5 w-32' alt="logoimg" />
+            <p className='w-full md:w-2/3 text-gray-600'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Footer
