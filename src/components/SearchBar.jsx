5c764bc7dad25a354a7cd2c0b608513a49afa368
Klement Tamás <klemitomi84@gmail.com>
2025-06-23 00:35:47 +0200

    Finish Searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useContext } from 'react';
 import { assets } from '../assets/frontend_assets/assets';
 import { ShopContext } from '../context/ShopContext';
+import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
 
 const SearchBar = () => {
 
     const { search, setSearch, showSearch, setShowSearch } = useContext(ShopContext);
-  return showSearch ? (
+    const [visible, setVisible] = useState(false);
+    const location = useLocation();
+
+    useEffect(() => {
+        if (location.pathname.includes('collection')) {
+            setVisible(true);
+        } else {
+            setVisible(false);
+        }
+    }, [location]);
+
+  return showSearch && visible ? (
     <div className='border-t border-b bg-gray-50 text-center'>
         <div className='inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 rounded-full w-3/4 sm:w-1/2'>
         <input 
