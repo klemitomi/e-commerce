a963c2f90dc183abb306aa638d76941900813ec2
Klement Tamás <klemitomi84@gmail.com>
2025-06-29 00:07:12 +0200

    add Cart page

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
 import { products } from "../assets/frontend_assets/assets"; 
 import { useState } from "react";
+import { toast } from "react-toastify";
 
 export const ShopContext = createContext();
 
@@ -10,10 +11,51 @@ const ShopContextProvider = (props)=> {
     const delivery_fee = 10;
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = async (itemId, size) => {
+        if (!size) {
+            toast.error('Please select a size before adding to cart!');
+            return;
+    }
+
+      let cartData = structuredClone(cartItems);
+      
+      if (cartData[itemId]) {
+        if (cartData[itemId][size]) {
+          cartData[itemId][size] += 1;
+        } else {
+          cartData[itemId][size] = 1;
+        }
+      } else {
+        cartData[itemId] = {};
+        cartData[itemId][size] = 1;       
+      }
+
+        setCartItems(cartData);
+    } 
+
+
+    const getCartCount = () => {
+        let totalCount = 0;
+        for (const items in cartItems) {
+            for(const item in cartItems[items]) {
+                try {
+                    if (cartItems[items][item] > 0) {
+                        totalCount += cartItems[items][item];
+                    }
+                } catch (error) {
+                    
+                }
+            }
+        }
+        return totalCount;
+    }
 
     const value = {
         products, currency, delivery_fee,
-        search, setSearch, showSearch, setShowSearch 
+        search, setSearch, showSearch, setShowSearch,
+        cartItems, setCartItems, addToCart, getCartCount
     }
 
     return (
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useContext, useState } from 'react';
+import { ShopContext } from '../context/ShopContext';
+import Title from '../components/Title';
+import ProductItem from '../components/ProductItem';
 
 const Cart = () => {
+
+  const { products, cartItems, currency } = useContext(ShopContext);
+
+  const [cartData, setCartData] = useState([]);
+
+  useEffect(() => {
+    const tempData = [];
+    for (const itemId in cartItems) {
+      for (const size in cartItems[itemId]) {
+        if (cartItems[itemId][size] > 0) {
+          tempData.push({
+            _id: itemId,
+            size: size,
+            quantity: cartItems[itemId][size]
+          });
+        }
+      }
+    }
+    setCartData(tempData);
+  }, [cartItems])
+
   return (
-    <div>
-      
+    <div className="border-t pt-14">
+     <div className="text-2xl mb-3">
+      <Title text1={'YOUR'} text2={'CART'} />
+     </div>
+     <div>
+       {
+        cartData.map((item, index) => {
+          const product = products.find(product => product._id === item._id);
+
+          return (
+            <div key={index} className="py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] gap-4 items-center">
+              <div className="flex items-start gap-6">
+                <img src={product.image[0]} alt='productimage' className="w-16 sm:w-20" />
+                <div className="flex flex-col">
+                  <h2 className="text-xs font-medium sm:text-lg">{product.name}</h2>
+                </div>
+                <div className='flex items-center gap-5 mt-2'>
+                  <p>{currency}{product.price}</p>
+                </div>
+              </div>
+              <div className="flex items-center">
+                <p className="text-sm">Qty: {item.quantity}</p>
+                <p className="px-2 sm:px-3 sm:py-1 border bg-slate-50">{item.size}</p>
+              </div>
+            </div>
+          )
+        })
+       }
+     </div>
+
     </div>
   )
 }
